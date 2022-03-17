ad2e0e38482e3cd7b595985d16b89d795e34c5e8
toniprotofy <toni@protofy.xyz>
2022-03-17 16:46:16 +0100

    adding classNames to create different sass

--- client-commerce/layouts/BasicLayout/BasicLayout.js
+++ client-commerce/layouts/BasicLayout/BasicLayout.js
@@ -1,10 +1,17 @@
 import React from "react";
 import { Button, Container } from "semantic-ui-react";
 import Header from "../../components/Header";
+import classNames from "classnames";
+
 export default function BasicLayout(props) {
-  const { children } = props;
+  const { children, className } = props;
   return (
-    <Container fluid className="basic-layout">
+    <Container
+      fluid
+      className={classNames("basic-layout", {
+        [className]: className,
+      })}
+    >
       <Header />
       <Container className="content">{children}</Container>
     </Container>
